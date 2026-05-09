6b560f0ec0d6d2ca9cf306b15e9a971872316170
Tapetal <aminuashraf55@gmail.com>
2026-05-09 18:38:51 +0100

    feat: implement landing nav and hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Afacad, Dancing_Script } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { env } from "@/env/client";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const afacad = Afacad({ subsets: ["latin"], variable: "--font-afacad" });
+const dancing = Dancing_Script({ subsets: ["latin"], variable: "--font-dancing" });
 
 export const metadata: Metadata = {
   metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
@@ -23,9 +24,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full antialiased font-sans", inter.variable)}
+      className={cn("h-full antialiased font-afacad", afacad.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`min-h-full flex flex-col ${afacad.variable} ${dancing.variable}`}>{children}</body>
     </html>
   );
 }
